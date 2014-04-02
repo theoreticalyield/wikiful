6c6a8899cae16953ac31401fffac60bd2ed89d75
David Hill <davidandhill@gmail.com>
2014-04-02 00:14:30 -0700

    Create Categories Controller along with index and show views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	
 	def index
 		@articles = Article.order(updated_at: :desc).limit(25)
 	end
@@ -8,8 +9,21 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
+		@article = Article.new
+	end
+		
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			redirect_to @article
+		else
+			render "new"
+		end
 	end
 
-	def create
-	end
+	private
+		def article_params
+			params.require(:article).permit(:title, :content, :category_ids => [])
+		end
+
 end
---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,11 @@
+class CategoriesController < ApplicationController
+
+	def index
+		@categories = Category.all.order [name: :asc]
+	end
+
+	def show
+		@category = Category.find(params[:id])
+	end
+
+end
